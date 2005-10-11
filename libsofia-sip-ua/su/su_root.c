50f304c0a5e40a8a17ac13de729b76a641f578d4
Pekka Pessi <Pekka.Pessi@nokia.com>
2005-10-11 17:57:20 +0300

    cvs ci su_root.c
    darcs-hash:20051011145720-65a35-3abbbbee90b9ce856c981ab011cf29437e59f076.gz

--- libsofia-sip-ua/su/su_root.c
+++ libsofia-sip-ua/su/su_root.c
@@ -30,13 +30,13 @@
  * @author Pekka Pessi <Pekka.Pessi@nokia.com>
  * 
  * @date Created: Tue Sep 14 15:51:04 1999 ppessi
- * $Date: 2005/10/11 09:02:37 $
+ * $Date: 2005/10/11 14:27:31 $
  */
 
 #include "config.h"
 
 const char su_root_c_id[] =
-"$Id: su_root.c,v 1.5 2005/10/11 09:02:37 ppessi Exp $";
+"$Id: su_root.c,v 1.6 2005/10/11 14:27:31 ppessi Exp $";
 
 #include <stdlib.h>
 #include <assert.h>
