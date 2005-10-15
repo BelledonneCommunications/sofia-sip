0d5057569912b77d2878acc9c4de72b520a14458
Pekka Pessi <Pekka.Pessi@nokia.com>
2005-10-15 20:22:24 +0300

    Removed sl_utils.c
    darcs-hash:20051015172224-65a35-584e8dca97d9d0610c56812a3dd7ea01a3e284a4.gz

---
+++ libsofia-sip-ua/nta/sl_utils.c
@@ -0,0 +1,42 @@
+/*
+ * This file is part of the Sofia-SIP package
+ *
+ * Copyright (C) 2005 Nokia Corporation.
+ *
+ * Contact: Pekka Pessi <pekka.pessi@nokia.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ */
+
+/**@ingroup sl_utils
+ *
+ * @CFILE sl_utils.c  Implementation of SIP library utility string functions.
+ *
+ * @author Pekka Pessi <Pekka.Pessi@nokia.com>
+ *
+ * @date Created:  Thu Oct  5 15:38:39 2000 ppessi
+ */
+
+#include "config.h"
+
+#include <stddef.h>
+#include <string.h>
+#include <stdio.h>
+
+#include <sip.h>
+
+#include "sl_utils.h"
