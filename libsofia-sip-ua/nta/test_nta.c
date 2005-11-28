74ed148e3768eb7debb4eac172a09aa4fb69e491
Pekka Pessi <Pekka.Pessi@nokia.com>
2005-11-28 19:34:56 +0200

    Explicitly binding nta to IPv6 when using IPv6 (test_nta.c).
    darcs-hash:20051128173456-65a35-ecd9f9c34ae60042f65f3bbcefd805d397aa8bf0.gz

--- libsofia-sip-ua/nta/test_nta.c
+++ libsofia-sip-ua/nta/test_nta.c
@@ -490,7 +490,7 @@ int test_init(agent_t *ag, char const *resolv_conf)
   TEST_1(ag->ag_mclass = msg_mclass_clone(sip_default_mclass(), 0, 0));
 
   if (str0cmp(getenv("ipv6"), "true") == 0) {
-    contact = "sip:*:*;comp=sigcomp";
+    contact = "sip:[::]:*;comp=sigcomp";
     af = AF_INET6, sulen0 = sizeof (struct sockaddr_in6);
   }
   else {
