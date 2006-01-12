406f5067a758e566878fc5d21f1e37af39016b1a
Martti Mela <martti.mela@nokia.com>
2006-01-12 10:49:28 +0200

    autogen works now with OS X 10.3 with port
    darcs-hash:20060112084928-1b897-9cf711918873b72465cd1a127a0266ff04b6a312.gz

--- autogen.sh
+++ autogen.sh
@@ -3,8 +3,17 @@
 set -x
 
 aclocal -I m4 &&
-autoheader &&
-libtoolize --force &&
+autoheader 
+
+if libtoolize --force 2> /dev/null
+  then echo using libtoolize
+elif glibtoolize --force 2> /dev/null
+  then echo using glibtoolize
+else
+  echo "error: no libtoolize or glibtoolize found."
+  exit -1
+fi
+
 automake --add-missing --copy --include-deps --foreign &&
 autoconf
 
