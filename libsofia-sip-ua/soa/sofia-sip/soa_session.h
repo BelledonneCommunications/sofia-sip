ed1e94cbc9da848e1cd6cc59f65444adee57ef47
Pekka Pessi <Pekka.Pessi@nokia.com>
2006-07-26 17:37:27 +0300

    sofia-sip/soa_session.h: added pad to bitfield.
    darcs-hash:20060726143727-65a35-f506779a7deb0b0be0cff8c9e16c9eaf77dbeb23.gz

--- libsofia-sip-ua/soa/sofia-sip/soa_session.h
+++ libsofia-sip-ua/soa/sofia-sip/soa_session.h
@@ -221,6 +221,8 @@ struct soa_session
     ss_srtp_confidentiality:1,
     ss_srtp_integrity:1;
 
+  unsigned :0;			/* Pad */
+
   int             ss_wcode;	/**< Warning code from last media operation */
   char const     *ss_warning;	/**< Warnings text from last media operation */
 };
