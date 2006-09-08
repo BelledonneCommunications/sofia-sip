c2c58cceecc2b48da5d9844deb666570aba2ca7a
Kai Vehmanen <kai.vehmanen@nokia.com>
2006-09-08 12:03:30 +0300

    Fixed a segfault handling an invalid tagtype in su_taglist.
    darcs-hash:20060908090330-7659e-d4effc1a304db47dca0afe1883dac2e6f999cbb6.gz

--- libsofia-sip-ua/su/su_taglist.c
+++ libsofia-sip-ua/su/su_taglist.c
@@ -372,7 +372,7 @@ int tl_gets(tagi_t const lst[], tag_type_t tag, tag_value_t value, ...)
       n += tl_get(tt, (void *)t->t_value, lst);
     }
 #if !defined(NDEBUG)
-    else if (tt->tt_class->tc_ref_set) {
+    else if (tt->tt_class && tt->tt_class->tc_ref_set) {
       fprintf(stderr, "WARNING: tag %s::%s directly used by tl_gets()\n",
 	      tt->tt_ns, tt->tt_name);
       assert(tt->tt_class == ref_tag_class);
