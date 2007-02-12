8e59b2896372f5f8f53ee658a5814642078d5e5a
Kai Vehmanen <kai.vehmanen@nokia.com>
2007-02-13 01:10:38 +0200

    nua: Ignore CANCEL of incoming request if we have already sent a final response, part 2.
    darcs-hash:20070212231038-7659e-af3d8e7848e33807a37ce091c2bc3d512b6c6801.gz

--- libsofia-sip-ua/nua/nua_session.c
+++ libsofia-sip-ua/nua/nua_session.c
@@ -2198,10 +2198,11 @@ int process_cancel(nua_server_request_t *sr,
   nua_session_usage_t *ss = nua_dialog_usage_private(sr->sr_usage);
   msg_t *cancel = nta_incoming_getrequest_ackcancel(irq);
 
-  assert(nta_incoming_status(irq) < 200);
   assert(ss); assert(ss == nua_session_usage_get(nh->nh_ds)); (void)ss;
 
   if (nua_server_request_is_pending(sr)) {
+    assert(nta_incoming_status(irq) < 200);
+
     nua_stack_event(nh->nh_nua, nh, cancel, nua_i_cancel, SIP_200_OK, NULL);
 
     SR_STATUS1(sr, SIP_487_REQUEST_TERMINATED);
