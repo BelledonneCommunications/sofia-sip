b9a1dda1586c714fdd3a36b035b823dc048e371d
Pekka Pessi <first.last@nokia.com>
2007-02-26 16:12:30 +0200

    sip_header.h: added SIP_IS_ALLOWED() macro.
    darcs-hash:20070226141230-55b16-0c6ab00c73ec71e6c17e2bcb31831cf81c382f67.gz

--- libsofia-sip-ua/sip/sofia-sip/sip_header.h
+++ libsofia-sip-ua/sip/sofia-sip/sip_header.h
@@ -345,8 +345,14 @@ int sip_has_supported(sip_supported_t const *support, char const *feature);
 SOFIAPUBFUN
 int sip_has_feature(msg_list_t const *supported, char const *feature);
 
+/** Return true if the method is listed in @Allow header. */
 SOFIAPUBFUN int sip_is_allowed(sip_allow_t const *allow, 
 			       sip_method_t method, char const *name);
+
+/** Check if the well-known method is listed in @Allow header. @NEW_1_12_6 */
+#define SIP_IS_ALLOWED(allow, method) \
+  (sip_method_unknown < (method) && (method) < 32 && \
+   (allow) && ((allow)->k_bitmap & (1 << (method))) != 0)
 
 /* ---------------------------------------------------------------------------
  * Bitmasks for header classifications
