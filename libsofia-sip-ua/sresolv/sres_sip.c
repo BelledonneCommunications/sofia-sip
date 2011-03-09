6589b7d76bef4ebfc62a6bff55cd3dcf241b429e
Pekka Pessi <Pekka.Pessi@nokia.com>
2011-03-09 15:26:22 +0200

    sres_sip.c: check log level in sres_sip_append_result()
    Be lazy expanding binary addresses.
    
    MeeGo Coverity issue CID#11745

--- libsofia-sip-ua/sresolv/sres_sip.c
+++ libsofia-sip-ua/sresolv/sres_sip.c
@@ -1625,7 +1625,7 @@ sres_sip_append_result(sres_sip_t *srs,
       break;
   }
 
-  if ((srs->srs_canonname && srs->srs_numeric) || (SU_LOG->log_level >= 0)) {
+  if ((srs->srs_canonname && srs->srs_numeric) || (SU_LOG->log_level >= 5)) {
     unsigned port = 0;
     char const *lb = "", *rb = "";
 
@@ -1660,6 +1660,7 @@ sres_sip_append_result(sres_sip_t *srs,
 
   if (!srs->srs_canonname)
     canonname = NULL;
+
   if (canonname) {
     clen = strlen(canonname);
     if (clen && canonname[clen - 1] == '.')
@@ -1676,6 +1677,7 @@ sres_sip_append_result(sres_sip_t *srs,
   *ai = *result;
   ai->ai_next = NULL;
   ai->ai_addr = memcpy(ai + 1, ai->ai_addr, ai->ai_addrlen);
+
   if (canonname) {
     ai->ai_canonname = (char *)(ai->ai_addr) + ai->ai_addrlen;
     memcpy(ai->ai_canonname, canonname, clen - 1);
