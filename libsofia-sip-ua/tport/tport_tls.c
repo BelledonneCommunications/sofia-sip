7ad4ac475324a589b0adbbd6b121fa6024f9ed4d
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2012-12-19 10:43:42 +0100

    Fix TLS memory leak.

--- libsofia-sip-ua/tport/tport_tls.c
+++ libsofia-sip-ua/tport/tport_tls.c
@@ -404,14 +404,14 @@ void tls_free(tls_t *tls)
   if (!tls)
     return;
 
-  if (tls->con != NULL)
+  if (tls->con != NULL) {
     SSL_shutdown(tls->con);
+    SSL_shutdown(tls->con);
+    SSL_free(tls->con);
+  }
 
   if (tls->ctx != NULL && tls->type != tls_slave)
     SSL_CTX_free(tls->ctx);
-
-  if (tls->bio_con != NULL)
-    BIO_free(tls->bio_con);
 
   su_home_unref(tls->home);
 }
@@ -480,7 +480,6 @@ tls_t *tls_init_secondary(tls_t *master, int sock, int accept)
 
   if (tls) {
     tls->ctx = master->ctx;
-    tls->type = master->type;
     tls->accept = accept ? 1 : 0;
     tls->verify_outgoing = master->verify_outgoing;
     tls->verify_incoming = master->verify_incoming;
