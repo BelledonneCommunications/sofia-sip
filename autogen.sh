6c1e2232b4edb789665292f600609d0c2fd77b58
Guillaume BIENKOWSKI <guillaume.bienkowski@belledonne-communications.com>
2014-10-16 15:21:59 +0200

    Permit out of source autogen

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
-
 set -x
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+THEDIR=`pwd`
+cd $srcdir
+
 ${AUTORECONF:-autoreconf} -i
 find . \( -name 'run*' -o -name '*.sh' \) -a -type f | xargs chmod +x
 chmod +x scripts/*
+cd $THEDIR
